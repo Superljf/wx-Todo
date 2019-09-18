3e5ba4f6f665f588f1b7d7cf41f0f90aaeaba788
Superljf <317558348@qq.com>
2019-09-18 16:18:35 +0800

    2019/9/18

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -1,7 +1,7 @@
 var jsonData = require("../../data/news.js")
 Page({
   data: {
-      
+
   },
   onLoad: function () {
     this.setData({
@@ -13,10 +13,26 @@ Page({
       modalName: e.currentTarget.dataset.target
     })
   },
+  // 添加上级
   hideModal(e) {
     this.setData({
       modalName: null
     })
+
+  },
+  getUserInfo: function (e) {
+    console.log(e.detail.userInfo)
+    if (e.detail.userInfo) {
+      app.globalData.userInfo = e.detail.userInfo
+      this.setData({
+        showLogin: !this.data.showLogin,
+        userInfo: e.detail.userInfo
+      });
+    } else {
+      wx.switchTab({
+        url: '../index/index'
+      })
+    }
   },
   ChooseCheckbox(e) {
     let items = this.data.checkbox;
@@ -30,5 +46,22 @@ Page({
     this.setData({
       checkbox: items
     })
+  },
+  // 显示员工资料信息
+  showinfo() {
+    wx.navigateTo({
+      url: '../../common/info/info',
+      success: (result) => {
+        
+      },
+      fail: () => {},
+      complete: () => {}
+    });
+      
+  },
+  // 添加上下级
+  addleader() {
+    
   }
 })
+
